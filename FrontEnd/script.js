75e290e78e9211e8e09f280eb2c6d48a9013ed88
amrsafi78 <safi.amr78@gmail.com>
2026-03-24 09:39:47 +0100

    Ajout des filtres dynamiques par catégorie

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -21,13 +21,42 @@ gallery.appendChild(figure);
 });
 }
 
-
-
 fetch("http://localhost:5678/api/works")
 .then(response => response.json())
 .then(works => {
 allWorks = works;
 displayWorks(allWorks);
+})
+.catch(error => {
+console.error("Erreur :", error);
+});
+
+fetch("http://localhost:5678/api/categories")
+.then(response => response.json())
+.then(categories => {
+const filtersContainer = document.querySelector(".filters");
+filtersContainer.innerHTML = "";
+
+const buttonAll = document.createElement("button");
+buttonAll.innerText = "Tous";
+filtersContainer.appendChild(buttonAll);
+
+buttonAll.addEventListener("click", () => {
+displayWorks(allWorks);
+});
+
+categories.forEach(category => {
+const button = document.createElement("button");
+button.innerText = category.name;
+filtersContainer.appendChild(button);
+
+button.addEventListener("click", () => {
+const filteredWorks = allWorks.filter(
+work => work.categoryId === category.id
+);
+displayWorks(filteredWorks);
+});
+});
 })
 .catch(error => {
 console.error("Erreur :", error);
